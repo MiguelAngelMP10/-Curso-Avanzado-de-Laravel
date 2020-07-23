305815087e9a07b9af8234d6fc75a96eb4ba33b1
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-07-22 21:01:31 -0500

    Manejo de relaciones en bases de datos con Laravel

---
+++ tests/Unit/ProductTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Category;
+use App\Product;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+
+
+class ProductTest extends TestCase
+{
+    use RefreshDatabase;
+    public function test_a_product_belongs_to_category()
+    {
+
+        $category = factory(Category::class)->create();
+        $product = factory(Product::class)->create(['category_id' => $category->id]);
+
+        $this->assertInstanceOf(Category::class, $product->category);
+        $this->assertEquals($category->id, $product->category->id);
+    }
+}
