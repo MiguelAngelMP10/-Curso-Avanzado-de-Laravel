1f3d46f9639ddea62732417a0acc6bb0d4404bfd
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-07-24 20:25:55 -0500

    Programación de tareas

--- app/Console/Commands/SendNewsletterCommand.php
+++ app/Console/Commands/SendNewsletterCommand.php
@@ -8,49 +8,41 @@ use Illuminate\Console\Command;
 
 class SendNewsletterCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'send:newsletter {emails?*}';
+    protected $signature = 'send:newsletter
+                            {emails?*} : Correos Electronicos a los cuales enviar directamente
+                            {--s|schedule : Si debe ser ejecutado directamente o no}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Envia un correo electronico';
+    protected $description = 'Envia un correo electronico a todos los usuarios que hayan verificado su cuenta';
 
-
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
     public function handle()
     {
-        $emails = $this->argument('emails');
-        $builder =  User::query();
+        $userEmails = $this->argument('emails');
+        $schedule = $this->option('schedule');
 
-        if ($emails) {
-            $builder->whereIn("email", $emails);
+        $builder = User::query();
+
+        if ($userEmails) {
+            $builder->whereIn('email', $userEmails);
         }
 
+        $builder->whereNotNull('email_verified_at');
         $count = $builder->count();
 
         if ($count) {
-            $this->output->progressStart($count);
+            $this->info("Se enviaran {$count} correos");
 
-            $builder->whereNotNull('email_verified_at')
-                ->each(function (User $user) {
+            if ($this->confirm('¿Estas de acuerdo?') || $schedule) {
+                $this->output->progressStart($count);
+                $builder->each(function (User $user) {
                     $user->notify(new NewsletterNotification());
                     $this->output->progressAdvance();
                 });
-
-            $this->output->progressFinish();
-            $this->info("Se enviaron {$count} correos");
+                $this->output->progressFinish();
+                $this->info('Correos enviados');
+                return;
+            }
         }
-        $this->info('No se envio ningun correo');
+
+        $this->info('No se enviaron correos');
     }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\SendEmailVerificationReminderCommand;
+use App\Console\Commands\SendNewsletterCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        SendNewsletterCommand::class,
+        SendEmailVerificationReminderCommand::class,
     ];
 
     /**
@@ -24,7 +27,23 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('inspire')
+            ->evenInMaintenanceMode()
+            ->sendOutputTo(storage_path('inspire.log'))
+            ->everyMinute();
+
+        $schedule->call(function () {
+            echo 'Hola';
+        })->everyFiveMinutes();
+
+        $schedule->command(SendNewsletterCommand::class)
+            ->withoutOverlapping()
+            ->onOneServer()
+            ->mondays();
+
+        $schedule->command(SendEmailVerificationReminderCommand::class)
+            ->onOneServer()
+            ->daily();
     }
 
     /**
@@ -34,7 +53,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
